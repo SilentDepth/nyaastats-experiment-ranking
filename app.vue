369be65fd0dd61086617b8ae7ba2f6ada3dabcd8
SilentDepth <silentdepth@outlook.com>
2023-02-12 18:37:18 +0800

    resizable list

--- app.vue
+++ app.vue
@@ -38,7 +38,7 @@ function applyAdvice (advice: Advice) {
 
 <template lang="pug">
 div(class="min-h-screen bg-stone-200 dark:bg-black dark:text-stone-200 flex flex-col items-center")
-  main(class="my-auto w-screen md:w-auto max-w-[400px] md:max-w-unset p-3 md:flex md:flex-row")
+  main(class="my-10 w-screen md:w-auto max-w-[400px] md:max-w-unset p-3 md:flex md:flex-row")
     section(class="md:flex-1 md:max-w-[300px] mb-5 md:mb-0 md:mr-5")
       details(:open="isLargeScreen" class="space-y-3")
         summary(class="list-none flex")
@@ -48,8 +48,9 @@ div(class="min-h-screen bg-stone-200 dark:bg-black dark:text-stone-200 flex flex
             path(d="M8.25 15L12 18.75 15.75 15")
         ol.custom-counter(class="list-inside dark:text-neutral-300 text-sm space-y-2")
           li 点击标题可编辑
+            p(class="pl-5 pt-1") 可回车换行
           li 查询表达式支持以下用法
-            ul(class="pl-5 pt-2 space-y-2")
+            ul(class="pl-5 pt-1 space-y-1")
               li
                 code(class="text-xs") crafted.glass
                 br
@@ -63,7 +64,9 @@ div(class="min-h-screen bg-stone-200 dark:bg-black dark:text-stone-200 flex flex
                 br
                 | 指标匹配
           li 指标名即游戏统计项的简略写法，即
-            p(class="pl-5") <code>minecraft:used/minecraft:glass</code><br>↓<br><code>used.glass</code>
+            p(class="pl-5 pt-1") <code>minecraft:used/minecraft:glass</code><br>↓<br><code>used.glass</code>
+          li 拖动榜单底边可调整榜单行数
+            p(class="pl-5 pt-1") 最少 1 行，最多 20 行
           li 试一试这些有趣的榜单
             ul(class="pl-5 pt-2 space-y-2")
               li(v-if="loadingAdvices")
@@ -93,7 +96,7 @@ div(class="min-h-screen bg-stone-200 dark:bg-black dark:text-stone-200 flex flex
           template(v-else)
             | {{ `查询 (${hotkey})` }}
       TheList(:data="data" :total="total")
-  footer(class="p-3")
+  footer(class="mt-auto p-3")
     a(href="https://github.com/SilentDepth/nyaastats-experiment-ranking" target="_blank")
       svg(viewBox="0 0 24 24" class="w-6 h-6 fill-stone-600 dark:fill-neutral-400 hover:fill-stone-800 dark:hover:fill-neutral-300")
         path(d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z")
--- components/the-list.vue
+++ components/the-list.vue
@@ -15,11 +15,52 @@ defineProps({
 function formatNumber (num: number): string {
   return String(Math.round(num)).replaceAll(/(\d)(?=(?:\d{3})+(\.|$))/g, '$1,')
 }
+
+let $list = $ref<HTMLOListElement>()
+let listSize = $ref<number>()
+
+function startResize (ev: PointerEvent) {
+  const posList = [...$list!.querySelectorAll('li')].map($li => [$li.offsetTop, $li.offsetTop + $li.offsetHeight])
+  let startSize = $list!.offsetHeight
+  let startY = ev.screenY
+
+  document.documentElement.style.userSelect = 'none'
+  document.addEventListener('touchmove', preventDefault, { passive: false })
+  document.addEventListener('pointermove', resizing)
+  document.addEventListener('pointerup', stopResize)
+
+  function preventDefault (ev: Event) {
+    ev.preventDefault()
+  }
+
+  function resizing (ev: PointerEvent) {
+    const _size = Math.max(0, startSize + (ev.screenY - startY))
+    const idx = posList.findIndex(it => it[0] <= _size && _size <= it[1])
+    const [min, max] = posList.at(idx)!
+    if (idx === 0) {
+      listSize = max
+      return
+    }
+    const med = (min + max) / 2
+    listSize = _size < med ? min : max
+  }
+
+  function stopResize () {
+    document.documentElement.style.userSelect = ''
+    document.removeEventListener('touchmove', preventDefault)
+    document.removeEventListener('pointermove', resizing)
+    document.removeEventListener('pointerup', stopResize)
+  }
+}
 </script>
 
 <template lang="pug">
-div(class="")
-  ol.mc-box(class="bg-white dark:bg-neutral-800 overflow-hidden divide-y-2 divide-black/20 dark:divide-black/40")
+div(class="relative")
+  ol(
+    ref="$list"
+    class="bg-white dark:bg-neutral-800 overflow-hidden divide-y-2 divide-black/20 dark:divide-black/40"
+    :style="{ height: typeof listSize === 'number' ? listSize + 'px' : null }"
+  )
     li(v-for="(it, idx) of data || 20" class="relative h-10 px-3 flex items-center")
       template(v-if="data")
         span(class="relative z-1 flex-none w-7 text-stone-400 dark:text-neutral-500 text-sm") {{ idx + 1 }}
@@ -32,4 +73,10 @@ div(class="")
         )
           span(class="absolute right-0 inset-y-0 z-0 bg-stone-100 dark:bg-neutral-700/50" :style="{ width: it.value / total * 100 + '%' }")
     li(v-if="data?.length === 0" class="h-10 px-3 text-stone-400 dark:text-neutral-500 flex justify-center items-center") NO DATA
+  div(class="h-2")
+    div(
+      class="w-full h-2 absolute bottom-0 inset-x-0 bg-black/25 flex justify-center items-center space-x-1 hover:bg-green-600/75 active:bg-green-600/75 cursor-ns-resize"
+      @pointerdown="startResize"
+    )
+      div(v-for="_ of 3" class="w-[2px] h-[2px] bg-white rounded-full")
 </template>
